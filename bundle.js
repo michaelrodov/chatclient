0ef115c154a0eaef6ebe7b522e42e10e65112b54
Michael Rodov <rodov@hp.com>
2016-11-02 23:16:43 +0200

    bundle

--- bundle.js
+++ bundle.js
@@ -22095,6 +22095,10 @@
 	
 	var Charts = _interopRequireWildcard(_charts);
 	
+	var _Loader = __webpack_require__(/*! ./Loader.jsx */ 178);
+	
+	var _Loader2 = _interopRequireDefault(_Loader);
+	
 	function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
 	
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
@@ -22121,6 +22125,7 @@
 	            timeBetweenMsg: 0,
 	            avgLettersPerUser: 0,
 	            avgLettersAllUsers: 0,
+	            loader: true,
 	            wph: ["wph"],
 	            mph: ["mph"]
 	
@@ -22168,6 +22173,7 @@
 	                fontSize: '10px'
 	            };
 	            var messageList = this._buildMessageList();
+	            var loaderStatus = this.props.username && this.props.username != "" && this.state.loader ? "" : "hidden";
 	            return _react2.default.createElement(
 	                'div',
 	                { className: 'container--chat-page' },
@@ -22238,6 +22244,7 @@
 	                    { className: 'container--list-messages', ref: 'msgList' },
 	                    messageList
 	                ),
+	                _react2.default.createElement(_Loader2.default, { hidden: loaderStatus }),
 	                _react2.default.createElement(_InputBar2.default, { actionName: 'send',
 	                    placeholder: 'Type a message',
 	                    className: 'chat--input',
@@ -22334,6 +22341,7 @@
 	                method: 'GET'
 	            }, function (data, xhr) {
 	                _this3._addMessages(data);
+	                _this3.setState({ loader: false });
 	            }, function (err, xhr) {
 	                console.error(xhr.responseURL, xhr.status, xhr.statusText);
 	            });
@@ -22735,6 +22743,13 @@
 	        axis: {
 	            y: {
 	                show: false
+	            },
+	            x: {
+	                tick: {
+	                    format: function format(x) {
+	                        return x + "h ago";
+	                    }
+	                }
 	            }
 	        },
 	        legend: {
@@ -22750,14 +22765,66 @@
 	                blue: '#a5c04d'
 	            },
 	            columns: columns,
-	            type: 'bar',
-	            axes: {
-	                mph: 'y',
-	                wph: 'y2'
-	            }
+	            type: 'bar'
 	        }
 	    });
 	}
+
+/***/ },
+/* 178 */
+/*!***************************!*\
+  !*** ./src/js/Loader.jsx ***!
+  \***************************/
+/***/ function(module, exports, __webpack_require__) {
+
+	"use strict";
+	
+	Object.defineProperty(exports, "__esModule", {
+	    value: true
+	});
+	
+	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+	
+	var _react = __webpack_require__(/*! react */ 1);
+	
+	var _react2 = _interopRequireDefault(_react);
+	
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+	
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+	
+	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+	
+	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+	
+	var Loader = function (_React$Component) {
+	    _inherits(Loader, _React$Component);
+	
+	    function Loader(props) {
+	        _classCallCheck(this, Loader);
+	
+	        return _possibleConstructorReturn(this, (Loader.__proto__ || Object.getPrototypeOf(Loader)).call(this, props));
+	    }
+	
+	    _createClass(Loader, [{
+	        key: "render",
+	        value: function render() {
+	            return _react2.default.createElement(
+	                "div",
+	                { className: "spinner " + this.props.hidden },
+	                _react2.default.createElement("div", { className: "rect1" }),
+	                _react2.default.createElement("div", { className: "rect2" }),
+	                _react2.default.createElement("div", { className: "rect3" }),
+	                _react2.default.createElement("div", { className: "rect4" }),
+	                _react2.default.createElement("div", { className: "rect5" })
+	            );
+	        }
+	    }]);
+	
+	    return Loader;
+	}(_react2.default.Component);
+	
+	exports.default = Loader;
 
 /***/ }
 /******/ ]);
